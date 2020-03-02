106832f2395e0b46c1c7524bb97ac72d67db9fc0
biancaaltman <43017874+biancaaltman@users.noreply.github.com>
2020-03-01 20:16:45 -0700

    admin can now change security qs

--- src/app/mobile-settings/mobile-settings.page.ts
+++ src/app/mobile-settings/mobile-settings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MobileSettingsService } from '../services/mobile-settings.service';
+import {Storage} from '@ionic/storage';
+import {Router} from '@angular/router';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-mobile-settings',
@@ -7,9 +11,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileSettingsPage implements OnInit {
 
-  constructor() { }
+  private securityQ1: string;
+  private securityQ2: string;
+  private securityQ3: string;
+  private chatHoursToLive: number;
+  private displayUpdateButton: boolean;
+
+
+  constructor(private msService: MobileSettingsService,
+              private storage: Storage,
+              private router: Router,
+              public alertController: AlertController
+              ) { }
 
   ngOnInit() {
+
+    this.storage.get('authenticated').then((val) => {
+      if (val === 'false') {
+        this.router.navigate(['/login/']);
+      }
+    });
+
+    this.displayUpdateButton = false;
+    this.getSecurityQs();
+    this.getChatRoomHourSetting();
   }
 
+  getSecurityQs() {
+    this.msService.getSecurityQs().then((result) => {
+      this.securityQ1 = result.get('q1');
+      this.securityQ2 = result.get('q2');
+      this.securityQ3 = result.get('q3');
+    });
+  }
+
+  getChatRoomHourSetting() {
+    this.msService.getChatRoomHourSetting().then((result) => {
+      this.chatHoursToLive = result.get('hours');
+    });
+  }
+
+  showUpdateButton(location) {
+    this.displayUpdateButton = true;
+  }
+
+
+  async updateSecurityQuestion(securityQ): Promise<void> {
+    const alert = await this.alertController.create({
+      inputs: [
+        { name: 'newSecurityQ', placeholder: 'New Security Question'},
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        { text: 'Update',
+          handler: data => {
+            this.msService.updateSecurityQ(
+                data.newSecurityQ, securityQ
+            );
+            this.getSecurityQs();
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
