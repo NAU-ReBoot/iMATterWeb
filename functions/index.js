2d3995795da9b419fc88ad5ff5c21893fd4ec759
Victoria Leafgren <vml56@nau.edu>
2020-03-04 21:18:16 -0700

    added function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const admin=require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var newChat;
+const nodemailer = require('nodemailer');
+admin.initializeApp(functions.config().firebase);
+
+
+//admin.initializeApp();
+require('dotenv').config()
+
+const {SENDER_EMAIL, SENDER_PASS}= process.env;
+
+exports.sendRecoveryEmail=functions.firestore.document('provider_recovery_email/{docID}').onCreate((snap,context)=>{	
+	const data=snap.data();
+	let authData = nodemailer.createTransport({
+		host:'smtp.gmail.com',
+		port:587,
+		secure: false,
+		auth: {
+		  user: SENDER_EMAIL, 
+		  pass: SENDER_PASS
+		}
+		
+		
+});
+
+	authData.sendMail({
+		from: 'imatternotification@gmail.com',
+		to: data.email, // list of receivers
+		subject: "Imatter InfoDesk", // Subject line
+		text: "Here is your recovery code: " + data.code, // plain text body
+		html: "Here is your recovery code: " + data.code // html body
+		//res.send("sent");
+		}).then(res=>console.log('successfully sent that mail')).catch(err=>console.log(err));
+	});
