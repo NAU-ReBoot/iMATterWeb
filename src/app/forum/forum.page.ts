7a0398e0336f52f5ff1799883d7429de8d672ae8
biancaaltman <43017874+biancaaltman@users.noreply.github.com>
2020-03-23 19:42:01 -0700

    searchbar for info desk added

--- src/app/forum/forum.page.ts
+++ src/app/forum/forum.page.ts
@@ -3,6 +3,9 @@ import {Observable} from 'rxjs';
 import {Question, QuestionService} from '../services/infoDesk/question.service';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
+import {FormControl} from '@angular/forms';
+import { debounceTime, filter } from 'rxjs/operators';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-forum',
@@ -12,8 +15,18 @@ import { Router } from '@angular/router';
 export class ForumPage implements OnInit {
 
   private questions: Observable<Question[]>;
+  private searchbar: any;
+  public searchControl: FormControl;
+  public items: any;
 
-  constructor(private questionService: QuestionService, private router: Router, private storage: Storage) {
+  public questionList: any[];
+  public loadedQuestionList: any[];
+
+  constructor(private questionService: QuestionService,
+              private router: Router,
+              private storage: Storage,
+              private afs: AngularFirestore) {
+    this.searchControl = new FormControl();
   }
 
   ngOnInit() {
@@ -21,9 +34,33 @@ export class ForumPage implements OnInit {
       if (val === 'false') {
         this.router.navigate(['/login/']);
       }
+
+      this.afs.collection('questions', ref => ref.orderBy('timestamp', 'desc'))
+          .valueChanges({ idField: 'id' }).subscribe(questionList => {
+            this.questionList = questionList;
+            console.log(this.questionList);
+            this.loadedQuestionList = questionList;
+          });
     });
 
     this.questions = this.questionService.getQuestions();
+  }
+
+  initializeItems(): void {
+    this.questionList = this.loadedQuestionList;
+  }
+
+  filterQuestions(event) {
+    console.log('called');
+    this.initializeItems();
+
+    const searchInput = event.target.value;
+
+    if (searchInput) {
+      this.questionList = this.questionList.filter(currentQuestion => {
+        return(currentQuestion.title.toLowerCase().indexOf(searchInput.toLowerCase()) > -1);
+      });
+    }
   }
 
 }
