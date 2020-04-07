70f5705cf86f593561367656d3902c529eaae7b5
Jennie <jad554@nau.edu>
2020-04-07 01:48:06 -0700

    finalllyyyy quiz forms are workinggit add srcgit add src!

--- src/app/learningModule/quiz-modal/quiz-modal.page.ts
+++ src/app/learningModule/quiz-modal/quiz-modal.page.ts
@@ -51,60 +51,23 @@ export class QuizModalPage implements OnInit {
         correctAnswer: ['', Validators.compose([Validators.required])],
       }),
       modulePointsWorth: [''],
-      moduleActive: [''],
-      id: []
+      moduleActive: ['']
     });
-
-      /*this.quizForm = this.formBuilder.group({
-        questionText: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
-        pointsWorth: ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.pattern('[0-9]+([0-9]+)*')])],
-        choice1: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
-        choice2: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
-        choice3: [''],
-        choice4: [''],
-        correctAnswer: ['', Validators.compose([Validators.required])],
-      });*/
     }
 
   ngOnInit() { 
     //find the current quiz question that we're viewing and create the reference to it
-    /*for (var index = 0; index < this.currentLearningModule.moduleQuiz.length; index++)
+    for (var index = 0; index < this.currentLearningModule.moduleQuiz.length; index++)
     {
       if (this.currentLearningModule.moduleQuiz[index] === this.currentQuizQuestion)
       {
         this.indexOfQuestion = index;
-
-        console.log("INDEX: " + index);
-        console.log(this.currentLearningModule.moduleQuiz[index]);
         this.modalQuizQuestion = this.currentLearningModule.moduleQuiz[index];
 
         this.learningModuleForm.patchValue(this.currentLearningModule);
         this.learningModuleForm.patchValue({moduleQuiz: this.modalQuizQuestion});
-        
-        console.log(this.currentLearningModule.moduleQuiz);
       }
-    }*/
-
-    /*this.currentLearningModule.moduleQuiz.forEach(question => {
-
-      if (question === this.currentQuizQuestion)
-      {
-        this.modalQuizQuestion = question;
-        this.learningModuleForm.patchValue(this.currentLearningModule);
-        this.learningModuleForm.patchValue({moduleQuiz: this.modalQuizQuestion});
-        console.log(this.currentLearningModule.moduleQuiz);
-        console.log(this.modalQuizQuestion);
-        //this.learningModuleForm.patchValue(this.modalQuizQuestion);
-      }
-    });*/
-
-    //find the current quiz question that we're viewing and create the reference to it
-    this.currentLearningModule.moduleQuiz.forEach(question => {
-      if (question === this.currentQuizQuestion)
-      {
-        this.modalQuizQuestion = question;
-      }
-    });
+    }
    }
 
   dismiss() {
@@ -141,11 +104,12 @@ export class QuizModalPage implements OnInit {
 
   updateLearningModule()
   {
-    /*var arrayOfQuestions = [];
+    this.learningModuleForm.addControl('id', this.formBuilder.control(this.currentLearningModule.id));
+    var arrayOfQuestions = [];
 
     if (this.learningModuleForm.status == 'VALID')
     {
-      for (var index = this.currentLearningModule.moduleQuiz.length-1; index >= 0; index--)
+      for (var index = 0; index <  this.currentLearningModule.moduleQuiz.length; index++)
       {
         if (index != this.indexOfQuestion)
         {
@@ -153,26 +117,17 @@ export class QuizModalPage implements OnInit {
         }
         else if (index === this.indexOfQuestion)
         {
-          arrayOfQuestions.push(this.learningModuleForm.);
+          arrayOfQuestions.push(this.learningModuleForm.get('moduleQuiz').value);
         }
       }
 
-      console.log(arrayOfQuestions);
+      this.currentLearningModule.moduleQuiz = arrayOfQuestions;
 
-      this.learningModuleForm.patchValue({moduleQuiz: arrayOfQuestions});
-
-      console.log(this.learningModuleForm.value);
-
-      this.learningModuleService.updateLearningModule(this.learningModuleForm.value).then(() => 
+      this.learningModuleService.updateLearningModule(this.currentLearningModule).then(() => 
       {
         this.showToast('Quiz question updated!');
       });
-    }*/
-
-    this.learningModuleService.updateLearningModule(this.currentLearningModule).then(() => 
-      {
-        this.showToast('Quiz question updated!');
-      });
+    }
   }
 
   showToast(msg:string)
