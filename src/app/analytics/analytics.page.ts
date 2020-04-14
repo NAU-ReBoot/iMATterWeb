8f506dc71f199e4254663afeaf440f68617d92c6
cv456 <cv456@nau.edu>
2020-04-13 18:39:21 -0700

    bar worked entire time

--- src/app/analytics/analytics.page.ts
+++ src/app/analytics/analytics.page.ts
@@ -185,6 +185,14 @@ export class AnalyticsPage implements OnInit{
 
     }
 
+    pageStatsOn()
+    {
+      this.pageStatistics = true;
+      this.durationPage = false;
+      this.submitted = false;
+      this.durationSubmitted = false; 
+    }
+
     On()
     {
       this.submitted = true;
@@ -556,35 +564,35 @@ export class AnalyticsPage implements OnInit{
 
 
     createBarChart()
- {
+     {
 
-   this.myBarChart= new Chart(this.barChart.nativeElement,{
-     type:'bar',
-     data:{
-       labels: ["12:00AM", "1:00AM", "2:00AM", "3:00AM" , "4:00AM", "5:00AM" ,
-                 "6:00AM", "7:00AM" , "8:00AM" , "9:00AM", "10:00AM" , "11:00AM",
-                 "12:00PM", "1:00PM", "2:00PM", "3:00PM" , "4:00PM", "5:00PM" ,
-                 "6:00PM", "7:00PM" , "8:00PM" , "9:00PM", "10:00PM" , "11:00PM"],
-       datasets: [{
-         label: "Number of Time Notifications is Clicked",
-         data:[2.5, 3.8, 2, 6.9, 6.9, 7, 10, 0],
-         backgroundColor: 'rgb(147,112,219)',
-         borderColor: 'rgb(147,112,219)',
-         borderWidth:1
-       }]
-     },
-     options:{
-       scales:{
-         yAxes:[{
-           ticks:{
-             beginAtZero:true
+       this.myBarChart= new Chart(this.barChart.nativeElement,{
+         type:'bar',
+         data:{
+           labels: ["12:00AM", "1:00AM", "2:00AM", "3:00AM" , "4:00AM", "5:00AM" ,
+                     "6:00AM", "7:00AM" , "8:00AM" , "9:00AM", "10:00AM" , "11:00AM",
+                     "12:00PM", "1:00PM", "2:00PM", "3:00PM" , "4:00PM", "5:00PM" ,
+                     "6:00PM", "7:00PM" , "8:00PM" , "9:00PM", "10:00PM" , "11:00PM"],
+           datasets: [{
+             label: "Number of Time Notifications is Clicked",
+             data:[2.5, 3.8, 2, 6.9, 6.9, 7, 10, 0],
+             backgroundColor: 'rgb(147,112,219)',
+             borderColor: 'rgb(147,112,219)',
+             borderWidth:1
+           }]
+         },
+         options:{
+           scales:{
+             yAxes:[{
+               ticks:{
+                 beginAtZero:true
+               }
+             }]
            }
-         }]
-       }
+         }
+       });
+       this.myBarChart.update();
      }
-   });
-   this.myBarChart.update();
- }
 
 
 
