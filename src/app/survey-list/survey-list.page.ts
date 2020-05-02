c9747b1f3ce136993b610e0f7d8dd454e0f4c642
Jennie <jad554@nau.edu>
2020-05-01 21:35:02 -0700

    send notifications now for surveys works

--- src/app/survey-list/survey-list.page.ts
+++ src/app/survey-list/survey-list.page.ts
@@ -1,8 +1,12 @@
+/// <reference types="@types/gapi.auth2" />
+
 import { Component, OnInit } from '@angular/core';
 import { FireService, Survey } from '../services/fire/fire.service';
 import { Observable } from 'rxjs';
 import {Storage} from '@ionic/storage';
 import {Router} from '@angular/router';
+
+declare var gapi: any;
 
 @Component({
   selector: 'app-survey-list',
@@ -33,6 +37,34 @@ export class SurveyListPage implements OnInit {
 
     this.surveys = this.fs.getSurveys();
 
+    gapi.load("client:auth2", function() {
+      gapi.auth2.init({client_id: "173430196657-73pv7jdl40pdldfqhacq1f96kfrio0ki.apps.googleusercontent.com"});
+    });
+
   }
 
+  authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/cloud-platform"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  loadClient() {
+    gapi.client.setApiKey("AIzaSyBuzbsBUVWvgyqvc3hiUrDWLMMSsCf3a0E");
+    return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/cloudscheduler/v1/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  execute() {
+    return gapi.client.cloudscheduler.projects.locations.jobs.run({
+      "name": "projects/techdemofirebase/locations/us-central1/jobs/survey_notification",
+      "resource": {}
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
 }
